3d7372f190881536c065a39bba23b59af73ac31a
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2024-11-18 12:15:44 +0000

    changed to UDP sockets

--- src/nmsAgent.java
+++ src/nmsAgent.java
@@ -1,160 +1,114 @@
 import java.io.*;
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
 import java.net.*;
-import java.time.LocalTime;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.nio.charset.StandardCharsets;
 
 import PDU.NetTask;
 
 public class nmsAgent {
-    private Socket socket;
-    private DataOutputStream output;
-    private DataInputStream input;
+    private DatagramSocket socket;
+    private InetAddress serverAddress;
+    private int serverPort;
 
     public nmsAgent(String servidorIP, int porta) throws IOException {
-        this.socket = new Socket(servidorIP, porta);
-        this.output = new DataOutputStream(socket.getOutputStream());
-        this.input = new DataInputStream(socket.getInputStream());
+        this.socket = new DatagramSocket(serverPort);
+        this.serverAddress = InetAddress.getByName(servidorIP);
+        this.serverPort = porta;
     }
 
-    // Método para enviar um array de bytes (não está a ser usado!! mudar código
-    // para isto)
     public void sendByteArray(byte[] data) throws IOException {
-        output.write(data); // Em seguida, envia os dados
-        output.flush(); // Garante que os dados sejam enviados
+        DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, serverPort);
+        socket.send(packet);
     }
 
-    // Método para receber um array de bytes
     public byte[] receiveByteArray() throws IOException {
-        int length = input.readInt(); // Lê o comprimento dos dados
-        byte[] buffer = new byte[length];
-
-        // Lê os dados no buffer
-        input.readFully(buffer); // readFully garante que todos os bytes sejam lidos
-
-        return buffer;
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+        return Arrays.copyOfRange(packet.getData(), 0, packet.getLength());
     }
 
     public void register() {
         try {
-            NetTask handler = new NetTask(); // criado o handler que vai criar os nossos pacotes para enviar
-            byte[] registerPDU = handler.createRegisterPDU(); // cria um pacote do tipo REGISTER
-
-            byte[] pduUUIDBytes = Arrays.copyOfRange(registerPDU, 0, 36); // copia os primeiros 36 bytes do pacote de
-                                                                          // forma a obter os bytes do UUID
-            String pduUUID = new String(pduUUIDBytes, StandardCharsets.UTF_8); // transforma os bytes em String usando
-                                                                               // StandardCharsets - confirmar se está
-                                                                               // correto!
-
-            // envio do pacote
+            NetTask handler = new NetTask();
+            byte[] registerPDU = handler.createRegisterPDU();
             sendByteArray(registerPDU);
+            System.out.println("Pedido de registro enviado.");
 
-            // debuging
-            System.out.println("Pedido de registro enviado com UUID: " + pduUUID);
+            // Receber resposta
+            byte[] response = receiveByteArray();
+            byte type = response[response.length - 1];
+            int typeInt = Byte.toUnsignedInt(type);
 
-            // Receber resposta do servidor
-            byte[] buffer = new byte[37];
-            input.read(buffer);
-
-            byte type = buffer[36]; // obtém o byte correspondente ao tipo
-            int typeInt = Byte.toUnsignedInt(type); // transforma o byte num tipo INT
-            System.out.println("O valor de type é: " + typeInt); // debuging
-
-            if (typeInt == NetTask.ACKNOWLEDGE) { // compara se o pacote recebido é do tipo ACK
-                System.out.println(
-                        "ACK recebido do servidor. Registro bem-sucedido!\nIP server : " + socket.getInetAddress());
+            if (typeInt == NetTask.ACKNOWLEDGE) {
+                System.out.println("[ACK] ACK recebido. Registro bem-sucedido.");
+            } else {
+                System.out.println("Registro falhou. Tipo recebido: " + typeInt);
             }
-
         } catch (IOException e) {
-            System.out.println("Erro ao enviar pedido de registro: " + e.getMessage());
-            e.printStackTrace();
+            System.out.println("Erro ao registrar: " + e.getMessage());
         }
     }
 
     public void receiveTasks() {
-        try (DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-                DataInputStream input = new DataInputStream(socket.getInputStream())) {
-
-            System.out.println("Aguardando tarefas...");
+        try {
+            System.out.println("Aguardando por tarefas...");
 
             while (true) { // Loop infinito para ficar a espera de dados
-                if (input.available() > 0) { // Verifica se há dados disponíveis na stream
-                    byte[] defaultBuffer = new byte[100];
-                    int bytesRead = input.read(defaultBuffer);
-                    System.out.println("Li estes bytes: " + bytesRead);
+                byte[] defaultBuffer = receiveByteArray();
+                int bytesRead = defaultBuffer.length;
 
-                    if (bytesRead > 0) {
-                        // Processa os dados recebidos
-                        byte[] bufferTemp = Arrays.copyOfRange(defaultBuffer, 0, 38);
+                if (bytesRead > 0) {
+                    // Processa os dados recebidos
+                    byte[] bufferTemp = Arrays.copyOfRange(defaultBuffer, 0, 38);
 
-                        String bufferPayloadData = new String(bufferTemp, StandardCharsets.UTF_8);
-                        int taskType = Byte.toUnsignedInt(bufferTemp[37]);
-                        System.out.println(taskType + " é este tipo de task");
+                    int taskType = Byte.toUnsignedInt(bufferTemp[37]);
 
-                        int payloadLength = 0;
-                        switch (taskType) {
-                            case 0:
-                                payloadLength = 2;
-                                break;
-                            case 1:
-                                payloadLength = 2;
-                                break;
-                            case 2:
-                                payloadLength = 9;
-                                break;
-                            case 3:
-                                payloadLength = 9;
-                                break;
-                            case 4:
-                                payloadLength = 5;
-                                break;
-                        }
-
-                        byte[] bufferPayload = Arrays.copyOfRange(defaultBuffer, 38, 38 + payloadLength);
-                        byte[] pduUUIDBytes = Arrays.copyOfRange(bufferTemp, 0, 36);
-                        String pduUUID = new String(pduUUIDBytes, StandardCharsets.UTF_8);
-                        int freq = Byte.toUnsignedInt(bufferPayload[0]);
-                        int threshold = Byte.toUnsignedInt(bufferPayload[1]);
-
-                        System.out.println("Recebi uma task do tipo: " + taskType + " com UUID: " + pduUUID);
-                        System.out.println("Recebi uma task c/ freq: " + freq + " e com threshold: " + threshold);
-
-                    } else {
-                        Thread.sleep(100);
+                    int payloadLength = 0;
+                    switch (taskType) {
+                        case 0:
+                            payloadLength = 2;
+                            break;
+                        case 1:
+                            payloadLength = 2;
+                            break;
+                        case 2:
+                            payloadLength = 9;
+                            break;
+                        case 3:
+                            payloadLength = 9;
+                            break;
+                        case 4:
+                            payloadLength = 5;
+                            break;
                     }
+
+                    byte[] bufferPayload = Arrays.copyOfRange(defaultBuffer, 38, 38 + payloadLength);
+                    byte[] pduUUIDBytes = Arrays.copyOfRange(bufferTemp, 0, 36);
+                    String pduUUID = new String(pduUUIDBytes, StandardCharsets.UTF_8);
+                    int freq = Byte.toUnsignedInt(bufferPayload[0]);
+                    int threshold = Byte.toUnsignedInt(bufferPayload[1]);
+
+                    System.out.println("Recebi uma task do tipo: " + taskType + " com UUID: " + pduUUID);
+                    System.out.println("Recebi uma task c/ freq: " + freq + " e com threshold: " + threshold);
+
+                } else {
+                    Thread.sleep(100);
                 }
             }
         } catch (IOException | InterruptedException e) {
-            System.out.println("Erro ao receber a task: " + e.getMessage());
-        }
-    }
-
-    public void executeTask(int taskType, byte[] payload) {
-        // Verifique o tipo de tarefa e execute ações
-        switch (taskType) {
-            case 0: // Por exemplo, uma tarefa de monitoramento de CPU
-                System.out.println("Iniciando monitoramento de CPU...");
-                break;
-            // Adicionar outros tipos de tarefas aqui
+            System.out.println("Erro ao receber tarefas: " + e.getMessage());
         }
     }
 
     public void closeConnection() {
-        try {
-            if (input != null)
-                input.close();
-            if (output != null)
-                output.close();
-            if (socket != null)
-                socket.close();
+        if (socket != null) {
+            socket.close();
             System.out.println("Conexão terminada\n");
-        } catch (IOException e) {
-            System.out.println("Erro ao fechar conexão: " + e.getMessage());
         }
     }
+    
 
     public static void main(String[] args) {
         String servidorIP = "127.0.0.1";
--- src/nmsServer.java
+++ src/nmsServer.java
@@ -1,142 +1,111 @@
 import java.io.*;
 import java.net.*;
-import java.time.LocalTime;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.json.simple.parser.ParseException;
 
 import PDU.NetTask;
 import parser.Json_parser;
 
 public class nmsServer {
-    private ServerSocket serverSocket;
-    private ConcurrentHashMap<Integer, InetAddress> agentRegistry;
+    private DatagramSocket socket;
+    private ConcurrentHashMap<Integer, InetSocketAddress> agentRegistry;
     private int agentCounter = 1;
-    private final ReentrantLock lock = new ReentrantLock();  // Lock to control thread execution order
-
 
     public nmsServer(int port) throws IOException {
-        this.serverSocket = new ServerSocket(port);
+        this.socket = new DatagramSocket(port);
         this.agentRegistry = new ConcurrentHashMap<>();
     }
 
     public void start() {
-        System.out.println("Servidor iniciado e aguardando conexões...");
+        System.out.println("Servidor iniciado e aguardando pacotes...");
+        byte[] buffer = new byte[1024];
+
         while (true) {
             try {
-                Socket socket = serverSocket.accept();
-                new Thread(() -> handleClient(socket)).start();
-                new Thread(() -> sendTasks(socket)).start();
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                new Thread(() -> handleClient(packet)).start();
+                //new Thread(() -> sendTasks()).start();
             } catch (IOException e) {
-                System.out.println("Erro ao aceitar conexão: " + e.getMessage());
+                System.out.println("Erro ao receber pacote: " + e.getMessage());
             }
         }
     }
 
-    private void handleClient(Socket socket) {
-        /*Antiga try condition:
-        ByteArrayOutputStream output = new ByteArrayOutputStream(socket.getOutputStream());
-        ByteArrayInputStream input = new ByteArrayInputStream(socket.getInputStream())
-                */ 
-        try (DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-             DataInputStream input =   new DataInputStream(socket.getInputStream())) {
-            /*
-            // Ler o pedido de registro do agente
-            byte[] registerPDU = (byte[]) input.readObject();
-            int type = (int) registerPDU[0];*/
+    private void handleClient(DatagramPacket packet) {
+        try {
+            byte[] data = packet.getData();
+            InetSocketAddress clientAddress = new InetSocketAddress(packet.getAddress(), packet.getPort());
+            int type = Byte.toUnsignedInt(data[data.length - 1]);
 
-            byte[] buffer = new byte[37];
-            input.read(buffer);
-
-            byte type = buffer[36];
-            int typeInt = Byte.toUnsignedInt(type);
-            System.out.println("O valor de type é: " + typeInt);
-            
-            if (typeInt == NetTask.REGISTER) {
-                int agentId = register(socket.getInetAddress());
-
+            if (type == NetTask.REGISTER) {
+                int agentId = register(clientAddress);
                 NetTask handler = new NetTask();
                 byte[] ackPDU = handler.createAckPDU();
 
-                output.write(ackPDU);
-                output.flush();
-                System.out.println("Agente registrado com sucesso. ID: " + agentId + "\nIP agent: " + socket.getInetAddress());
+                sendPacket(ackPDU, clientAddress);
+                System.out.println("Agente registrado. ID: " + agentId + " IP: " + clientAddress);
+                sendTasks();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao processar cliente: " + e.getMessage());
+        }
+    }
+
+    private void sendTasks() {
+        try {
+            Json_parser tasks = new Json_parser("src/tasks.json");
+            HashMap<Integer, byte[]> tasksMap = new HashMap<>();
+
+            try {
+                tasksMap = tasks.tasks_parser();
+            } catch (ParseException e) {
+                System.out.println("Erro ao processar o arquivo JSON: " + e.getMessage());
+                e.printStackTrace();
+                return; // Interrompe o envio de tarefas se o JSON estiver inválido
             }
 
-        } catch (IOException e) {
-        System.out.println("Erro ao comunicar com o cliente: " + e.getMessage());
-    }
-    }
-private void sendTasks(Socket socket) {
-    try (DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-         DataInputStream input = new DataInputStream(socket.getInputStream())) {
-
-        Json_parser tasks = new Json_parser("src/tasks.json");
-        HashMap<Integer, byte[]> tasksMap = new HashMap<>();
-        
-        try {
-            tasksMap = tasks.tasks_parser();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        // Loop para tentar enviar as tasks a cada 500ms
-        while (!tasksMap.isEmpty()) {  // Enquanto houver tarefas a serem enviadas
             for (Map.Entry<Integer, byte[]> entry : tasksMap.entrySet()) {
-                int agentID = entry.getKey(); // Obter o agentID da task
-                byte[] taskPDU = entry.getValue(); // Conteúdo da task
+                int agentID = entry.getKey();
+                byte[] taskPDU = entry.getValue();
 
-                System.out.println("Tentando enviar task para o agente ID -> " + agentID);
-
-                // Verifica se o agente está registrado no agentRegistry
                 if (agentRegistry.containsKey(agentID)) {
-                    InetAddress agentAddress = agentRegistry.get(agentID); // Obtém o endereço IP do agente
+                    InetSocketAddress clientAddress = agentRegistry.get(agentID);
 
-                    // Verifica se o socket está conectado ao agente correto
-                    if (socket.getInetAddress().equals(agentAddress)) {
-                        // Envia a task para o agente correspondente (já no socket existente)
-                        System.out.println("Enviando pacote para o agente ID " + agentID + " no IP: " + agentAddress);
-                        output.write(taskPDU, 0, taskPDU.length);
-                        output.flush();
-                        System.out.println("Pacote enviado com sucesso ao agente " + agentID);
+                    sendPacket(taskPDU, clientAddress);
+                    System.out.println("Task enviada para o agente ID " + agentID);
 
-                        // Após enviar a task com sucesso, remove a task do mapa
-                        tasksMap.remove(agentID);
-                        System.out.println("Task removida do mapa para o agente ID " + agentID);
-                    } else {
-                        System.out.println("Este socket não corresponde ao agente " + agentID + ". Task não enviada.");
-                    }
+                    // Após enviar a task com sucesso, remove a task do mapa
+                    tasksMap.remove(agentID);
+                    System.out.println("Task removida do mapa para o agente ID " + agentID);
                 } else {
-                    System.out.println("Agente com o ID " + agentID + " não foi registrado.");
+                    System.out.println("Agente " + agentID + " não registrado.");
                 }
             }
-
-            System.out.println("Esperando 500ms antes de tentar novamente...");
-            Thread.sleep(500);  // Aguarda 500ms antes de tentar enviar novamente
-
+        } catch (IOException e) {
+            System.out.println("Erro ao enviar tarefas: " + e.getMessage());
         }
-
-    } catch (IOException | InterruptedException e) {
-        System.out.println("Erro ao comunicar com os agentes: " + e.getMessage());
     }
-}
 
+    private void sendPacket(byte[] data, InetSocketAddress clientAddress) throws IOException {
+        DatagramPacket packet = new DatagramPacket(data, data.length, clientAddress.getAddress(),
+                clientAddress.getPort());
+        socket.send(packet);
+    }
 
-
-    private int register(InetAddress agentAddress) {
+    private int register(InetSocketAddress clientAddress) {
         int agentId = agentCounter++;
-        agentRegistry.put(agentId, agentAddress);
+        agentRegistry.put(agentId, clientAddress);
         return agentId;
     }
 
     public static void main(String[] args) {
-        int porta = 12345;
+        int port = 12345;
         try {
-            nmsServer server = new nmsServer(porta);
+            nmsServer server = new nmsServer(port);
             server.start();
         } catch (IOException e) {
             System.out.println("Erro ao iniciar o servidor: " + e.getMessage());
